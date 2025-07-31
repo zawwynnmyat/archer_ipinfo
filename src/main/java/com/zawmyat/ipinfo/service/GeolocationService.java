f4f543fc3b66b25905afbc4f770a65eacf18ab5e
Zaw Wynn Myat <94133457+zawwynnmyat@users.noreply.github.com>
2025-08-01 04:32:09 +0630

    Update GeolocationService.java

--- src/main/java/com/zawmyat/ipinfo/service/GeolocationService.java
+++ src/main/java/com/zawmyat/ipinfo/service/GeolocationService.java
@@ -9,7 +9,9 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 
 @Service
@@ -21,14 +23,17 @@ public class GeolocationService {
 
     @PostConstruct
     public void init() throws IOException {
-        File database = new File("src/main/resources/GeoLite2-City.mmdb");
-        dbReader = new DatabaseReader.Builder(database).build();
+        InputStream cityStream = getClass().getResourceAsStream("/GeoLite2-City.mmdb");
+        InputStream countryStream = getClass().getResourceAsStream("/GeoLite2-Country.mmdb");
+        InputStream asnStream = getClass().getResourceAsStream("/GeoLite2-ASN.mmdb");
 
-        File databaseCountry = new File("src/main/resources/GeoLite2-Country.mmdb");
-        dbCountryReader = new DatabaseReader.Builder(databaseCountry).build();
+        if (cityStream == null || countryStream == null || asnStream == null) {
+            throw new FileNotFoundException("One or more GeoLite2 database files not found in resources.");
+        }
 
-        File databaseASN = new File("src/main/resources/GeoLite2-ASN.mmdb");
-        dbAsnReader = new DatabaseReader.Builder(databaseASN).build();
+        dbReader = new DatabaseReader.Builder(cityStream).build();
+        dbCountryReader = new DatabaseReader.Builder(countryStream).build();
+        dbAsnReader = new DatabaseReader.Builder(asnStream).build();
     }
 
     public CityResponse getLocation(String ip) throws IOException, GeoIp2Exception {
